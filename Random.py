58d9555e3a12fe6a88cb20650addc3a6e1f47655
jweiler281 <bob.smith19741974@live.com>
2020-04-05 12:52:51 -0500

    Updated Random.py to correct test output

--- Random.py
+++ Random.py
@@ -30,17 +30,17 @@ def hourMult(hr,mi):
         #returns tuple with mult, ID, #hours
         #Updated to find accurate multipliers for realistic hourly activity
         if (hr == 24 or (hr >= 0 and hr <= 6)): #00:00 - 05:59
-                return (30,'A',7)
+                return (30,'A',7,0,6)
         elif (hr > 6 and hr <= 8): #06:00 - 07:59
-                return (200,'B',2)
+                return (200,'B',2,6,8)
         elif (hr > 8 and hr <= 16): #08:00 - 15:59
-                return (275,'C',8)
+                return (275,'C',8,8,16)
         elif (hr > 16 and hr <= 22): #16:00 - 21:59
-                return (350,'D',6)
+                return (350,'D',6,16,22)
         elif (hr > 22 and hr < 24): #22:00 - 23:59
-                return (100,'E',1)
+                return (100,'E',1,22,24)
         else:
-                return (215,'F',1) #Catches edge case, replace with error catching/exception handling
+                return (215,'F',1,25,25) #Catches edge case, replace with error catching/exception handling
 
 def timeCheck(multtC):
         #Takes input of multiplier (multtC) and spits out lower/uper values
@@ -62,8 +62,8 @@ def lightsOps():
         opsMultPerc = opsMult[0]*.001 #Stores mult as a % for statistic analysis purposes
         opsTC = timeCheck(opsMult) #Calls timeCheck(mult) to get randomly generated (r1, r2) values returned as tuple
         opsLights = lightsOn(opsTC,opsMult[0]) #Determines if the lights are to be on or off
-        #r1;r2;mult;mult%;multHours;Hour;Minute;LightsOn? --> Log Format
-        opsLogs = str(opsTC[0])+";"+str(opsTC[1])+";"+str(opsMult[0])+";"+str(opsMultPerc)+";"+str(opsMult[2])+";"+str(currhour.hour)+";"+str(currhour.minute)+";"+str(opsLights[1])
+        #r1;r2;mult;mult%;multHours;multBeginHour;multEndHour;Hour;Minute;LightsOn? --> Log Format
+        opsLogs = str(opsTC[0])+";"+str(opsTC[1])+";"+str(opsMult[0])+";"+str(opsMultPerc)+";"+str(opsMult[2])+";"+str(opsMult[3])+";"+str(opsMult[4])+";"+str(currhour.hour)+";"+str(currhour.minute)+";"+str(opsLights[1])
         csvWriter('RandomPyLogs.csv',opsLogs)
         return opsLights[1] #Yes or No for lights being on
 
@@ -78,14 +78,14 @@ def lightsOps():
 def testingResults():
         #Used for testing purposes only
         #Spits out numerical string of results for feedback
-        #r1;r2;mult;mult%;multHours;Hour;Minute;LightsOn?
+        #r1;r2;mult;mult%;multHours;multBeginHour;multEndHour;Hour;Minute;LightsOn?
         now_H_test = random.randint(0,24) #Randomly generates hour for testing
         now_M_test = random.randint(0,59) #Randomly generates minute for testing
         mult = hourMult(now_H_test, now_M_test) #Calls hourMult(hr,mi) to get applicable mult
         multPerc = mult[0]*.001 #Stores mult as a % for statistic analysis purposes
         tc = timeCheck(mult) #Calls timeCheck(mult) to get randomly generated (r1, r2) values returned as tuple
         lights = lightsOn(tc,mult[0]) #Determines if the lights are on in each scenario
-        return str(tc[0])+";"+str(tc[1])+";"+str(mult[0])+";"+str(multPerc)+";"+str(mult[2])+";"+str(currhour.hour)+";"+str(currhour.minute)+";"+str(lights[1])
+        return str(tc[0])+";"+str(tc[1])+";"+str(mult[0])+";"+str(multPerc)+";"+str(mult[2])+";"+str(mult[3])+";"+str(mult[4])+";"+str(now_H_test)+";"+str(now_M_test)+";"+str(lights[1])
         
 def manyIterations():
         #Used for testing purposes only
